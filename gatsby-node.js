d93529941c5d5bc65d5330a2f3de553d144f52c7
Carl Nygard <cjnygard@gmail.com>
2020-07-19 17:59:29 -0400

    add graceful-fs to try to get around the filehandle limit

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,4 +1,8 @@
 'use strict';
 
+const realFs = require('fs')
+const gracefulFs = require('graceful-fs')
+gracefulFs.gracefulify(realFs)
+
 exports.createPages = require('./gatsby/create-pages');
 exports.onCreateNode = require('./gatsby/on-create-node');
