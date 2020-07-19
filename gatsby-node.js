3d6eb08fca3654854c1e235c1b15242da55dc06b
Carl Nygard <cjnygard@gmail.com>
2020-07-19 18:22:48 -0400

    fixing yarn errors, adding GTM

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,8 +1,10 @@
 'use strict';
 
-const realFs = require('fs')
-const gracefulFs = require('graceful-fs')
-gracefulFs.gracefulify(realFs)
+const realFs = require('fs');
+
+const gracefulFs = require('graceful-fs');
+
+gracefulFs.gracefulify(realFs);
 
 exports.createPages = require('./gatsby/create-pages');
 exports.onCreateNode = require('./gatsby/on-create-node');
